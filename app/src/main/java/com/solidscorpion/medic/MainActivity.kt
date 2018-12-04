50590f34f150e5bce7765337626d2665962a0070
Anton Pripachkin <>
2018-12-04 15:47:31 +0200

    Basic layout

--- app/src/main/java/com/solidscorpion/medic/MainActivity.kt
+++ app/src/main/java/com/solidscorpion/medic/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.solidscorpion.medic
 
+import android.net.http.SslError
 import android.os.Bundle
+import android.webkit.SslErrorHandler
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.webkit.WebViewClientCompat
+import com.solidscorpion.medic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.webview.webViewClient = object : WebViewClientCompat() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                binding.toolbar.toolbar.title = url
+                binding.toolbar.toolbar.setTitleTextColor(ContextCompat.getColor(this@MainActivity, android.R.color.holo_blue_dark))
+                super.onPageFinished(view, url)
+            }
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                if (!request.hasGesture()) return false
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                handler?.proceed()
+                super.onReceivedSslError(view, handler, error)
+            }
+        }
+        val settings = binding.webview.settings
+        settings.javaScriptEnabled = true
+        binding.webview.loadUrl("https://dev.medic.co.il/")
+    }
+
+    override fun onBackPressed() {
+        if (binding.webview.canGoBack()) {
+            binding.webview.goBack()
+        }
+        super.onBackPressed()
     }
 }
