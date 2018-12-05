ae3df2785c22f30528f8f1f667651a97441817eb
Anton Pripachkin <>
2018-12-05 16:55:59 +0200

    added some styles and resources

--- app/src/main/java/com/solidscorpion/medic/MainActivity.kt
+++ app/src/main/java/com/solidscorpion/medic/MainActivity.kt
@@ -2,12 +2,16 @@ package com.solidscorpion.medic
 
 import android.net.http.SslError
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.webkit.SslErrorHandler
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.webkit.WebViewClientCompat
 import com.solidscorpion.medic.databinding.ActivityMainBinding
 
@@ -19,7 +23,6 @@ class MainActivity : AppCompatActivity() {
         binding.webview.webViewClient = object : WebViewClientCompat() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 binding.toolbar.toolbar.title = url
-                binding.toolbar.toolbar.setTitleTextColor(ContextCompat.getColor(this@MainActivity, android.R.color.holo_blue_dark))
                 super.onPageFinished(view, url)
             }
             override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
@@ -35,6 +38,40 @@ class MainActivity : AppCompatActivity() {
         val settings = binding.webview.settings
         settings.javaScriptEnabled = true
         binding.webview.loadUrl("https://dev.medic.co.il/?app")
+        setSupportActionBar(binding.toolbar.toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(ResourcesCompat.getDrawable(resources, R.drawable.ic_menu_black_24dp, theme))
+        }
+        binding.drawer.addDrawerListener(
+            object : DrawerLayout.DrawerListener {
+                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                    // Respond when the drawer's position changes
+                }
+
+                override fun onDrawerOpened(drawerView: View) {
+                    // Respond when the drawer is opened
+                }
+
+                override fun onDrawerClosed(drawerView: View) {
+                    // Respond when the drawer is closed
+                }
+
+                override fun onDrawerStateChanged(newState: Int) {
+                    // Respond when the drawer motion state changes
+                }
+            }
+        )
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                binding.drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onBackPressed() {
