d2c50e29faa2a68f1001da1bf7278a40fdb81c20
Anton Pripachkin <>
2018-12-06 14:53:57 +0200

    Added resources, modified drawer behaviour

--- app/src/main/java/com/solidscorpion/medic/MainActivity.kt
+++ app/src/main/java/com/solidscorpion/medic/MainActivity.kt
@@ -9,15 +9,13 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
-import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.webkit.WebViewClientCompat
 import com.solidscorpion.medic.adapter.RVAdapter
 import com.solidscorpion.medic.databinding.ActivityMainBinding
 import com.solidscorpion.medic.pojo.ModelMenuItem
-import kotlinx.android.synthetic.main.activity_main.view.*
+
 
 class MainActivity : AppCompatActivity(), MainActivityContract.View {
     override fun onMenuItemsLoaded(items: List<ModelMenuItem>) {
@@ -28,9 +26,9 @@ class MainActivity : AppCompatActivity(), MainActivityContract.View {
                     .append(it.link)
                     .append("?app")
                     .toString())
-            binding.drawer.closeDrawer(GravityCompat.START)
+           slideUp(binding.drawerLayout.drawerContainer)
         }
-        binding.drawerContainer.menu.adapter = adapter
+        binding.drawerLayout.menu.adapter = adapter
     }
 
 
@@ -63,34 +61,23 @@ class MainActivity : AppCompatActivity(), MainActivityContract.View {
             title = ""
             setHomeAsUpIndicator(ResourcesCompat.getDrawable(resources, R.drawable.ic_menu_black_24dp, theme))
         }
-        binding.drawer.addDrawerListener(
-                object : DrawerLayout.DrawerListener {
-                    override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-                        // Respond when the drawer's position changes
-                    }
-
-                    override fun onDrawerOpened(drawerView: View) {
-                        // Respond when the drawer is opened
-                    }
-
-                    override fun onDrawerClosed(drawerView: View) {
-                        // Respond when the drawer is closed
-                    }
-
-                    override fun onDrawerStateChanged(newState: Int) {
-                        // Respond when the drawer motion state changes
-                    }
-                }
-        )
-
-        binding.drawerContainer.menu.layoutManager = LinearLayoutManager(this)
+        binding.drawerLayout.closeDrawer.setOnClickListener { slideUp(binding.drawerLayout.drawerContainer) }
+        binding.drawerLayout.menu.layoutManager = LinearLayoutManager(this)
         presenter.loadMenuItems()
+    }
+
+    private fun slideUp(view: View) {
+        view.animate().translationY(-view.height.toFloat()).setDuration(500).start()
+    }
+
+    private fun slideDown(view: View) {
+        view.animate().translationY(0f).setDuration(500).start()
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
             android.R.id.home -> {
-                binding.drawer.openDrawer(GravityCompat.START)
+                slideDown(binding.drawerLayout.drawerContainer)
                 true
             }
             else -> super.onOptionsItemSelected(item)
