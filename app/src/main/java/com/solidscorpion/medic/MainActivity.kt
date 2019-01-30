d6b1af78fcfd16da2e4523d1236b5e77de0dadce
li11041989 <EqdNFZc8mY>
2019-01-30 19:17:40 +0200

    modifications per request

--- app/src/main/java/com/solidscorpion/medic/MainActivity.kt
+++ app/src/main/java/com/solidscorpion/medic/MainActivity.kt
@@ -115,14 +115,6 @@ class MainActivity : AppCompatActivity(), MainActivityContract.View, AppBarLayou
                 handler?.proceed()
                 super.onReceivedSslError(view, handler, error)
             }
-
-            override fun onReceivedHttpError(view: WebView?, request: WebResourceRequest?, errorResponse: WebResourceResponse?) {
-                super.onReceivedHttpError(view, request, errorResponse)
-            }
-
-            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceErrorCompat?) {
-                super.onReceivedError(view, request, error)
-            }
         }
         binding.toolbar.autocomplete.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
             if (hasFocus)
