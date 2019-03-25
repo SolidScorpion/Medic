10bf9a13141a7756f8f257e8f671acf0a600575a
li11041989 <EqdNFZc8mY>
2019-03-25 21:15:07 +0200

    fixes per request

--- app/src/main/java/com/solidscorpion/medic/MainActivity.kt
+++ app/src/main/java/com/solidscorpion/medic/MainActivity.kt
@@ -218,7 +218,6 @@ class MainActivity : AppCompatActivity(), MainActivityContract.View, AppBarLayou
                 hideKeyboard()
                 if (binding.webview.url == "https://medic.co.il/?app") {
                     setCustomToolbar(FULL_TOOLBAR_HEIGHT_DP)
-
                 }
                 slideDown(binding.drawerLayout.drawerContainer)
                 true
@@ -456,9 +455,10 @@ class MainActivity : AppCompatActivity(), MainActivityContract.View, AppBarLayou
         binding.webview.loadUrl("javascript:loginPopup.signOut()")
     }
 
-    override fun userLogged() {
+    override fun userLogged()  {
         loggedUser = true
-        binding.btnUser.setImageDrawable(getDrawable(R.drawable.ic_user_logged))
+//        binding.btnUser.setImageDrawable(getDrawable(R.drawable.ic_user_logged))
+        binding.btnUser.visibility = View.GONE
         presenter.loadMenuItems(true)
     }
 
